821bdf89fef444d51140d2d22bc963f505be872e
Ri Ram <eram1294@gmail.com>
2016-08-09 17:17:45 -0400

    Created shiny app, needs to be visually cleaned up

---
+++ HiddenPowerCalc/server.R
@@ -0,0 +1,25 @@
+#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+source("/Users/riram/Desktop/hidden-power-calc/calc.R")
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+   
+  output$type <- renderText({
+    get_hp_type(input$hp, input$atk, input$def, input$sp,
+                input$spa, input$spd)
+  })
+  
+  output$dmg <- renderText({
+    get_hp_damage(input$hp, input$atk, input$def, input$sp,
+                  input$spa, input$spd)
+  })
+  
+})
---
+++ HiddenPowerCalc/ui.R
@@ -0,0 +1,60 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Pokémon Hidden Power Calculator"),
+  
+  # Sidebar with a slider input
+  sidebarLayout(
+    sidebarPanel(
+       sliderInput("hp",
+                   "HP:",
+                   min = 0,
+                   max = 31,
+                   value = 31),
+       sliderInput("atk",
+                   "Attack:",
+                   min = 0,
+                   max = 31,
+                   value = 31),
+       sliderInput("def",
+                   "Defense:",
+                   min = 0,
+                   max = 31,
+                   value = 31),
+       sliderInput("spa",
+                   "Special Attack:",
+                   min = 0,
+                   max = 31,
+                   value = 31),
+       sliderInput("spd",
+                   "Special Defense:",
+                   min = 0,
+                   max = 31,
+                   value = 31),
+       sliderInput("sp",
+                   "Speed:",
+                   min = 0,
+                   max = 31,
+                   value = 31)
+    ),
+    
+    # Show text for calculated type and damage
+    mainPanel(
+       h3("Type:"),
+       textOutput("type"),
+       h3("Damage:"),
+       textOutput("dmg")
+    )
+  )
+))
